3d551c46ac6f1b131a846a0e977bb95301da82c0
MatheusNascimentodaSilva <matheus12mopaz@gmail.com>
2023-10-19 11:15:46 -0300

    init

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>academia</title>
+    <link  rel="stylesheet" href="assets/css/main.css">
+</head>
+
+
+        <body>  
+
+    <?php
+        include_once("assets/componentes/cabecalho.html");
+    ?>
+
+
+
+
+    <?php
+        include_once("assets/componentes/texto.html");
+    ?>
+
+
+        
+
+
+   
+    <?php
+        include_once("assets/componentes/corpo.html");
+    ?>
+
+        
+
+     <?php
+        include_once("assets/componentes/rodape.html");
+    ?>
+
+
+
+        </body>
+
+  
+
+</html>
