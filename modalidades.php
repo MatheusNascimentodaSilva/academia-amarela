77da49276f932058909b9569653cd09a3918e182
MatheusNascimentodaSilva <matheus12mopaz@gmail.com>
2023-10-23 09:37:35 -0300

    init

--- modalidades.php
+++ modalidades.php
@@ -5,11 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>academia</title>
-    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/css/css2.css" rel="stylesheet">
 </head>
 <body>
     <?php
         include_once("assets/componentes/cabecalho.html");
     ?>
+
+
+
+<?php
+        include_once("assets/componentes/modalidades.html");
+    ?>
+
+<?php
+        include_once("assets/componentes/rodape.html");
+    ?>
+
+
+
 </body>
 </html>
