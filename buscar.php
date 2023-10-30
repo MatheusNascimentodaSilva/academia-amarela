9391d52c473a34c1439187205b3d6ec81dc12c27
MatheusNascimentodaSilva <matheus12mopaz@gmail.com>
2023-10-30 10:35:48 -0300

    update

--- buscar.php
+++ buscar.php
@@ -6,20 +6,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de usuario</title>
-    <link  rel="stylesheet" href="assets/css/main.css">
+    <link  rel="stylesheet" href="assets/css/buscar.css">
 </head>
 <body>
     <?php
         include_once("assets/componentes/cabecalho.html");
     ?>
-     <form action="assets/db/pessoa/select.php">
-        <h2>Buscar usuário</h2>
-        <label>Telefone</label>
-        <input type="telefone" name="telefone">
-        <input type="submit" value="Buscar">
-    </form>
+    
+    <?php
+        include_once("assets/componentes/buscar.html");
+    ?>
+    
     <?php
         include_once("assets/componentes/rodape.html");
     ?>
+ 
 </body>
 </html>
