f2a54eb127d5a54383411261b4097e3bea014542
MatheusNascimentodaSilva <matheus12mopaz@gmail.com>
2023-11-09 09:05:05 -0300

    update

--- assets/db/pessoa/insert.php
+++ assets/db/pessoa/insert.php
@@ -8,7 +8,7 @@ $sobrenome = $_REQUEST['sobrenome'];
 $email = $_REQUEST['email'];
 $telefone = $_REQUEST['telefone'];
 $endereco = $_REQUEST['endereco'];
-$senha = $_REQUEST['senha'];
+$senha = md5 ($_REQUEST['senha']);
 
 
 $sql = "INSERT INTO pessoa (nome, sobrenome, email, telefone, endereco, senha) VALUES ('$nome','$sobrenome', '$email', '$telefone', '$endereco', '$senha')";
